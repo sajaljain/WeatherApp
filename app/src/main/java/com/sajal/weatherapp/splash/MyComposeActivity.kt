929ef9ae79dc71b953fdcbd61c6676826b74a940
Sajal Jain1 <sajal.jain1@timesinternet.in>
2023-11-03 13:00:57 +0530

    Step 2 : Image composable

--- app/src/main/java/com/sajal/weatherapp/splash/MyComposeActivity.kt
+++ app/src/main/java/com/sajal/weatherapp/splash/MyComposeActivity.kt
@@ -1,24 +1,16 @@
 package com.sajal.weatherapp.splash
 
 import android.os.Bundle
-import android.widget.ImageButton
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
-import com.sajal.weatherapp.splash.ui.theme.WeatherAppTheme
-import kotlin.math.max
+import com.sajal.weatherapp.R
 
 class MyComposeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,15 +22,12 @@ class MyComposeActivity : ComponentActivity() {
 
     @Composable
     fun sayCheezy(name : String = "Some Default Name Value") {
-        Text(text = "$name",
-            fontStyle = FontStyle.Italic,
-            fontWeight = FontWeight.ExtraBold,
-            color = Color.Red,
-            fontSize = 35.sp,
-            textAlign = TextAlign.Center,
-            maxLines = 2,
-            fontFamily = FontFamily.Serif
-            )
+        Image(
+            painter = painterResource(id = R.drawable.heart_16),
+            contentDescription = "Heart Image",
+            colorFilter = ColorFilter.tint(Color.Red),
+            contentScale = ContentScale.Inside
+        )
     }
 
     @Preview(showBackground = true, name = "Hello Message", widthDp = 200, heightDp = 200)

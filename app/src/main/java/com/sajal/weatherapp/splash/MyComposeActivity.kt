2df0902e0445c13c4202960fca9b88a4945f8886
Sajal Jain1 <sajal.jain1@timesinternet.in>
2023-11-04 11:19:51 +0530

    Step 3 : Button composable

--- app/src/main/java/com/sajal/weatherapp/splash/MyComposeActivity.kt
+++ app/src/main/java/com/sajal/weatherapp/splash/MyComposeActivity.kt
@@ -4,10 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.sajal.weatherapp.R
@@ -22,15 +23,19 @@ class MyComposeActivity : ComponentActivity() {
 
     @Composable
     fun sayCheezy(name : String = "Some Default Name Value") {
-        Image(
-            painter = painterResource(id = R.drawable.heart_16),
-            contentDescription = "Heart Image",
-            colorFilter = ColorFilter.tint(Color.Red),
-            contentScale = ContentScale.Inside
-        )
+        Button(
+            onClick = { }, colors = ButtonDefaults.buttonColors(
+                contentColor = Color.White,
+                containerColor = Color.Cyan
+            ),
+            enabled = false
+        ) {
+            Text(text = "$name")
+            Image(painter = painterResource(id = R.drawable.heart_16), contentDescription = "heart")
+        }
     }
 
-    @Preview(showBackground = true, name = "Hello Message", widthDp = 200, heightDp = 200)
+    @Preview(showBackground = true, name = "Preview Message", widthDp = 200, heightDp = 200)
     @Composable
     private fun myPreviewFunction(){
         sayCheezy("Hello Sajal")

dbeeca6a6c30bb25592efebe30478677921685ec
Sajal Jain1 <sajal.jain1@timesinternet.in>
2023-11-15 09:55:17 +0530

    Step 4 : Text Field composable

--- app/src/main/java/com/sajal/weatherapp/splash/MyComposeActivity.kt
+++ app/src/main/java/com/sajal/weatherapp/splash/MyComposeActivity.kt
@@ -6,10 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import com.sajal.weatherapp.R
 
@@ -17,28 +22,41 @@ class MyComposeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            sayCheezy("Hello World")
+            myInputTextField()
         }
     }
 
     @Composable
-    fun sayCheezy(name : String = "Some Default Name Value") {
+    fun buttonComposable(name: String = "Some Default Name Value") {
         Button(
             onClick = { }, colors = ButtonDefaults.buttonColors(
-                contentColor = Color.White,
-                containerColor = Color.Cyan
-            ),
-            enabled = false
+                contentColor = Color.White, containerColor = Color.Cyan
+            ), enabled = false //button enabled or disabled
         ) {
             Text(text = "$name")
             Image(painter = painterResource(id = R.drawable.heart_16), contentDescription = "heart")
         }
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    private fun myInputTextField() {
+        var state = remember {
+            mutableStateOf("")
+        }
+        TextField(
+            value = TextFieldValue(text = state.value),
+            onValueChange = { state.value = it.toString() },
+            placeholder = { "My Placeholder value" },
+            label = { "My Label" }
+        )
+
+    }
+
     @Preview(showBackground = true, name = "Preview Message", widthDp = 200, heightDp = 200)
     @Composable
-    private fun myPreviewFunction(){
-        sayCheezy("Hello Sajal")
+    private fun myPreviewFunction() {
+        myInputTextField()
     }
 
 }
